b86e162af95f8443fa0bb4b5b9f6c900ac41f956
lonewolfgames <nathanfaucett@gmail.com>
2014-07-30 22:36:33 -0500

    added image, fixed delta time on stop and start

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 (function speedometer(window, document, undefined) {
     var performance = window.performance || {},
+        RATIO = 1.4310344827586208,
         RATE = 1000 / 60,
         START_MS, now;
 
@@ -43,12 +44,24 @@
         window.clearTimeout
     );
 
+    function loadImage(src, callback) {
+        var image = new Image();
+
+        image.onload = function() {
+            callback(null, image);
+        };
+        image.error = function() {
+            callback(new Error("404 Not found"));
+        };
+
+        image.src = src;
+    }
+
     /*
      * Speedometer(options Object)
      *   options {
      *     id: String (document id selector) uses getElementById to find, defaults to "speedometer"
-     *     width: Number width of speedometer, defaults to 256
-     *     height: Number height of speedometer, defaults to 256
+     *     height: Number height of speedometer, defaults to 256 width based off height to maintain ratio
      *     total: max number the speedometer can be, defaults to 100
      *   }
      */
@@ -58,31 +71,45 @@
 
         this.element = document.getElementById((typeof(options.id) === "string" ? options.id : "speedometer"));
 
-        this.width = (options.width = +options.width) ? options.width : 256;
         this.height = (options.height = +options.height) ? options.height : 256;
+        this.width = this.height * RATIO | 0;
+
+        this.backgroundColor = typeof(options.backgroundColor) === "string" ? options.backgroundColor : "#ddd";
+        this.barColor = typeof(options.barColor) === "string" ? options.barColor : "#ff8800";
 
         this.canvas = null;
         this.ctx = null;
 
         this.total = ((options.total = +options.total) && options.total > 0) ? options.total : 100;
         this.value = 0;
-        this.percent = 0.0;
-        this.current = 0.0;
-        this.difference = 0.0;
+
+        this.diff = 0;
+        this.dir = 1;
+        this.to = 0;
+        this.from = 0;
+        this.current = 0;
+
+        this.startAngle = Math.PI * 0.875;
+        this.currentStartAngle = this.startAngle;
+        this.currentEndAngle = this.startAngle + (Math.PI * 1.125);
+
+        this.arrowStartAngle = Math.PI * -0.63;
+        this.arrowAngle = 0;
 
         this.dt = 1 / 60;
+        this._current = 0;
 
+        this._image = null;
         this._run = false;
         this._requestId = null;
 
         var _this = this,
-            current = 0,
-            last = -1 / 60;
+            last = 0;
 
         this._update = function _update() {
-            last = current;
-            current = now();
-            _this.dt = (current - last) * 0.001;
+            last = _this._current;
+            _this._current = now();
+            _this.dt = (_this._current - last) * 0.001;
 
             _this.draw();
 
@@ -91,31 +118,50 @@
             }
         }
 
-        return this.init(((options.value = +options.value) && options.value > 0) ? (options.value <= this.total ? options.value : this.total) : 0);
+        return this.init(
+            ((options.value = +options.value) && options.value > 0) ? (options.value <= this.total ? options.value : this.total) : 0
+        );
     }
 
     Speedometer.prototype.init = function(value) {
-        var canvas = document.createElement("canvas"),
+        var _this = this,
+            canvas = document.createElement("canvas"),
             ctx = canvas.getContext("2d");
 
         canvas.width = this.width;
         canvas.height = this.height;
 
         ctx.translate(this.width * 0.5, this.height * 0.5);
-        ctx.scale(1, -1);
 
         this.canvas = canvas;
         this.ctx = ctx;
 
         this.element.appendChild(canvas);
 
-        this.set(value);
+        loadImage("./img/speedometer.png", function(err, image) {
+
+            _this._image = image;
+            _this.set(value);
+        });
+
+        return this;
+    };
+
+    Speedometer.prototype.clear = function() {
+        var canvas = this.canvas;
+
+        this.element = null;
+        this.ctx = null;
+        this.canvas = null;
+
+        canvas.parentNode.removeChild(canvas);
 
         return this;
     };
 
     Speedometer.prototype.start = function() {
         if (!this._requestId) {
+            this._current = now();
             this._run = true;
             this._update();
         }
@@ -137,14 +183,23 @@
      */
     Speedometer.prototype.set = function(value) {
         var total = this.total,
-            prev = this.value;
+            prev = this.value,
+            diff;
 
         value = +value;
         value = value < 0 ? 0 : (value > total ? total : value);
 
         this.value = value;
-        this.percent = value / total;
-        this.difference = (value - prev) / total;
+
+        this.to = value / total;
+        this.from = prev / total;
+        diff = this.to - this.from;
+
+        this.diff = diff;
+        this.dir = diff < 0 ? -1 : 1;
+        this.current = this.from;
+
+        console.log(this.current);
 
         this.start();
 
@@ -153,48 +208,91 @@
 
     Speedometer.prototype.draw = function() {
         var ctx = this.ctx,
-            radius = (this.height - 2) * 0.5;
+            dt = this.dt,
+            w = this.width,
+            h = this.height,
+            hw = w * 0.5,
+            hh = h * 0.5,
+            image = this._image,
+            current = 0;
 
-        this.current += this.difference * this.dt;
-        if (this.current >= this.difference) this.stop();
+        console.log(this.current, this.diff, dt);
 
-        ctx.clearRect(-this.width * 0.5, -this.height * 0.5, this.width, this.height);
+        this.current += this.diff * dt;
+        current = this.current;
 
-        this.drawArrow();
+        console.log(this.current, this.diff, dt);
+
+        if (this.dir === 1) {
+            if (current >= this.to) this.stop();
+        } else {
+            if (current <= this.to) this.stop();
+        }
+
+        ctx.clearRect(-hw, -hh, w, h);
 
         ctx.save();
 
+        ctx.drawImage(image, 0, 0, image.width, image.height, -hw, -hh, w, h);
+        ctx.globalCompositeOperation = "source-in";
+
+        ctx.fillStyle = this.backgroundColor;
+        ctx.fillRect(-hw, -hh, w, h);
+
+        ctx.globalCompositeOperation = "source-atop";
+
+        drawBar(this, ctx, dt, w, h);
+
+        ctx.globalCompositeOperation = "source-over";
+
+        drawArrow(this, ctx, dt, w, h);
+
+        ctx.restore();
+
+        return this;
+    };
+
+    function drawBar(_this, ctx, dt, w, h) {
+        var offset = h * 0.215,
+            lineWidth = w * 0.125,
+            r = (h * 0.9) - lineWidth,
+            x = 0,
+            y = offset;
+
+        ctx.save();
+
+        ctx.strokeStyle = _this.barColor;
+        ctx.lineWidth = lineWidth;
+
         ctx.beginPath();
-        ctx.arc(0, 0, radius, Math.PI * -0.125, Math.PI * -0.8625, false);
+        ctx.arc(x, y, r, 0, Math.PI, false);
         ctx.stroke();
 
         ctx.restore();
+    }
 
-        return this;
-    };
-
-    Speedometer.prototype.drawArrow = function() {
-        var ctx = this.ctx,
-            width = (this.width - 2) * 0.0425,
-            height = (this.height - 2) * 0.5;
+    function drawArrow(_this, ctx, dt, w, h) {
+        var offset = h * 0.215,
+            x = 0,
+            y = offset;
 
         ctx.save();
 
-        ctx.fillStyle = "#ddd";
+        ctx.translate(x, y);
+        ctx.rotate(0);
+        ctx.translate(-x, -y);
 
-        ctx.rotate((Math.PI * 0.625) - (this.current * Math.PI * 1.25));
+        ctx.fillStyle = _this.backgroundColor;
 
         ctx.beginPath();
-        ctx.moveTo(0, height);
-        ctx.lineTo(-width, 0);
-        ctx.lineTo(width, 0);
-        ctx.closePath();
+        ctx.moveTo(-w * 0.025, y);
+        ctx.lineTo(w * 0.025, y);
+        ctx.lineTo(w * 0.005, y - h * 0.5);
+        ctx.lineTo(-w * 0.005, y - h * 0.5);
         ctx.fill();
 
         ctx.restore();
-
-        return this;
-    };
+    }
 
     window.Speedometer = Speedometer;
 
